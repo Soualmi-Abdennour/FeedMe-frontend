d62c8e6c95e1126b8a6726fab97ccb98e805506e
Soualmi Abdennour <soualmiabdennour2005@gmail.com>
2026-05-30 03:42:10 +0100

    Refactor AppSideBar logo container structure

--- src/components/organism/AppSideBar.tsx
+++ src/components/organism/AppSideBar.tsx
@@ -53,8 +53,8 @@ function AppSidebar() {
             overflow-hidden
         ">
             {/* Logo */}
-            <div className="p-3 flex items-center justify-center h-[72px] shrink-0">
-<div className="w-10 h-10  rounded-full shrink-0">
+           <div className="p-3 flex items-center justify-center h-[72px] shrink-0">
+                <div className="w-10 h-10  rounded-full shrink-0">
                     <Image 
                         src={"/logo.svg"}
                         alt="FeedMe logo"
@@ -63,7 +63,8 @@ function AppSidebar() {
                         className="w-full h-full object-cover rounded-full"
                     >
                     </Image>
-                </div>            </div>
+                </div>
+            </div>
 
             <div className="flex-1 flex flex-col justify-between overflow-hidden">
                 {/* Navigation Items */}
